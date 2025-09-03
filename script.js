fd62cbb8969e265d3b21e487903a82de852e28ea
alexandrafs5 <alexandra051009@gmail.com>
2025-09-03 15:24:42 -0700

    Replace the constructor and prototype methods with class syntax

--- script.js
+++ script.js
@@ -24,16 +24,17 @@ bookForm.addEventListener("submit", function (event) {
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
